f46517c8b14a35baf6c00e7f667aab49f512e2f3
miguelgisbert12 <miguelgisbert12@alumnos.cei.es>
2025-04-15 00:59:20 +0200

    Intento 1 de subir el backend a Render

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
-console.log('NODE_ENV:', process.env.NODE_ENV);
-
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true
 }));
 app.use(express.json());
